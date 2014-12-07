32ca9c1ebe3281f97dc159232c1e8bd99605e0b5
Cameron White <camw85@mac.com>
2014-12-06 19:30:42 -0700

    tie queue to tranlator view

--- client/app/translator/translator.js
+++ client/app/translator/translator.js
@@ -4,7 +4,7 @@ angular.module('transitApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('translator', {
-        url: '/translator',
+        url: '/translator/:thingId',
         templateUrl: 'app/translator/translator.html',
         controller: 'TranslatorCtrl'
       });
